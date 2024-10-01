4f9d74f697af5136e97334ba638e13a1abf32dcf
Jacob Casas <jacobcasas17@gmail.com>
2024-10-01 11:10:46 -0700

    initial commmit

---
+++ main.js
@@ -0,0 +1,26 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const paragraphs = document.querySelectorAll('.text-content');
+
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute("data-visible") 
+        ? navToggle.setAttribute("aria-expanded", false)
+        : navToggle.setAttribute("aria-expanded", true);
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay");
+});
+
+// Function to fetch and display text file content
+paragraphs.forEach(paragraph => {
+    const file = paragraph.getAttribute('data-file');
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            // Insert the text into the p tag
+            paragraph.innerText = data;
+        })
+        .catch(error => {
+            console.error('Error loading file:', error);
+        });
+});
